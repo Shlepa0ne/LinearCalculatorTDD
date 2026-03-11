83339508b1731de9a7d85a696f4ec600970ae49a
Aleksey <89061963710@mail.ru>
2026-03-11 20:40:02 +0700

    Add test for Gauss method on simple 2x2 system

--- test_solver.py
+++ test_solver.py
@@ -4,3 +4,12 @@ from solver import Solver
 def test_solver_creation():
     solver = Solver()
     assert isinstance(solver, Solver)
+
+def test_gauss_simple_2x2():
+    solver = Solver()
+    A = [[2, 1],
+         [1, 2]]
+    b = [5, 4]
+    expected = [2, 1]
+    result = solver.solve_gauss(A, b)
+    assert result == pytest.approx(expected)
